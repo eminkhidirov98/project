9bf9a321cf2badc0e6f76ffed407c39a567072e1
Emin <eminkhidirov98@gmail.com>
2025-01-17 19:25:51 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18")
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
